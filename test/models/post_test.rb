19d817348cac3b58c62e1bad322b8025af14e478
Ashal aka JOKER <ahastudio@gmail.com>
2016-01-02 00:54:19 +0900

    테스트 코드 추가

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'create a new post with valid attributes' do
+    assert_difference -> { Post.count }, 1 do
+      Post.create(title: '제ㅋ목ㅋ', body: '내ㅋ용ㅋ')
+    end
+  end
+
+  test 'cannot create a new post with invalid attributes' do
+    assert_no_difference -> { Post.count } do
+      Post.create(title: '', body: '')
+    end
+  end
 end
