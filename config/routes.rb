e1ac49dd22fd51815761234ed261119e3c619419
Ashal aka JOKER <ahastudio@gmail.com>
2018-12-13 01:06:52 +0900

    Upgrade to Rails 5.0.7.1
    Update Rails version: 5.0.0.beta1 -> 5.0.7.1
    
    http://railsdiff.org/5.0.0.beta1/5.0.7.1
    
    Un-use jQuery.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,4 @@ Rails.application.routes.draw do
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
 end
